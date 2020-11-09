6fc2e933d9f9b2bd5d69c0df7a7f92ec6dd73496
Lieselotte <52315535+she3py@users.noreply.github.com>
2020-11-09 01:38:24 +0100

    Implement BukkitColorMap

--- src/fr/she3py/iplacer/ImagePlacer.java
+++ src/fr/she3py/iplacer/ImagePlacer.java
@@ -1,7 +1,31 @@
 package fr.she3py.iplacer;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ImagePlacer extends JavaPlugin {
+	public static ImagePlacer plugin;
+	public static Logger logger;
 	
+	@Override
+	public void onEnable() {
+		plugin = this;
+		logger = this.getLogger();
+		
+		Bukkit.getScheduler().runTaskAsynchronously(this, () -> {
+			try {
+				BukkitColorMap colorMap = BukkitColorMap.build();
+				colorMap.mapImage("in.png", "out.png");
+				
+				logger.info(colorMap.getData().toString());
+			}
+			catch(IOException e) {
+				logger.log(Level.SEVERE, "", e);
+			}
+		});
+	}
 }
